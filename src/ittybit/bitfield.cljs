2d63fbf6044a538446386ebc37dea8d930e43690
Alan O'Donnell <alan.m.odonnell@gmail.com>
2014-05-11 17:03:10 -0400

    Add simple immutable bitfield type

--- src/ittybit/bitfield.cljs
+++ src/ittybit/bitfield.cljs
@@ -1,33 +1,48 @@
-(ns ittybit.bitfield
-  (:refer-clojure :exclude [get set]))
+(ns ittybit.bitfield)
 
-(defn make
-  [size]
-  (js/Buffer. (js/Array. (+ 1 (bit-shift-right size 3)))))
+(deftype Bitfield [buf]
+  ICollection
+  (-conj [this i]
+    (let [mask (bit-shift-right 2r10000000 (rem i 8))
+          old-byte (aget buf (bit-shift-right i 3))
+          new-byte (bit-or mask old-byte)
+          buf' (js/Buffer. (.-length buf))]
+      (. buf (copy buf'))
+      (aset buf' (bit-shift-right i 3) new-byte)
+      (Bitfield. buf')))
+  ILookup
+  (-lookup [this i]
+    (-lookup this i nil))
+  (-lookup [this i not-found]
+    (let [mask (bit-shift-right 2r10000000 (rem i 8))
+          the-byte (aget buf (bit-shift-right i 3))]
+      (if-not (zero? (bit-and mask the-byte))
+        i
+        not-found)))
+  ISet
+  (-disjoin [this i]
+    (let [mask (bit-not (bit-shift-right 2r10000000 (rem i 8)))
+          old-byte (aget buf (bit-shift-right i 3))
+          new-byte (bit-and mask old-byte)
+          buf' (js/Buffer. (.-length buf))]
+      (. buf (copy buf'))
+      (aset buf' (bit-shift-right i 3) new-byte)
+      (Bitfield. buf')))
+  IFn
+  (-invoke [this i]
+    (-lookup this i))
+  (-invoke [this i not-found]
+    (-lookup this i not-found)))
 
-(defn get
-  [bf i]
-  (let [mask (bit-shift-right 2r10000000 (rem i 8))
-        the-byte (aget bf (bit-shift-right i 3))]
-    (not (zero? (bit-and mask the-byte)))))
+(defn of-size [size]
+  (let [buf (js/Buffer. (js/Array. (+ 1 (bit-shift-right size 3))))]
+    (Bitfield. buf)))
 
-(defn set
-  [bf i]
-  (let [mask (bit-shift-right 2r10000000 (rem i 8))
-        old-byte (aget bf (bit-shift-right i 3))
-        new-byte (bit-or mask old-byte)]
-    (aset bf (bit-shift-right i 3) new-byte)))
-
-(defn unset
-  [bf i]
-  (let [mask (bit-not (bit-shift-right 2r10000000 (rem i 8)))
-        old-byte (aget bf (bit-shift-right i 3))
-        new-byte (bit-and mask old-byte)]
-    (aset bf (bit-shift-right i 3) new-byte)))
-
-(let [bf (make 1)]
+(let [bf (of-size 1)]
   (assert (not (get bf 0)))
-  (set bf 1)
-  (assert (get bf 1))
-  (unset bf 1)
-  (assert (not (get bf 1))))
+  (let [bf' (conj bf 0)]
+    (assert (nil? (bf 0)))
+    (assert (= 0 (bf' 0)))
+    (let [bf'' (disj bf' 0)]
+      (assert (= 0 (bf' 0)))
+      (assert (nil? (bf'' 1))))))
