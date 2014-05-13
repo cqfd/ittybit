4056f4ef22f1e2a5809c2fc2a2140a4c49326dec
Alan O'Donnell <alan.m.odonnell@gmail.com>
2014-05-12 20:00:23 -0400

    Keep track of Bitfield size

--- src/ittybit/bitfield.cljs
+++ src/ittybit/bitfield.cljs
@@ -1,6 +1,7 @@
 (ns ittybit.bitfield)
 
 (deftype Bitfield [buf]
+(deftype Bitfield [size buf]
   ICollection
   (-conj [this i]
     (let [mask (bit-shift-right 2r10000000 (rem i 8))
@@ -9,7 +10,7 @@
           buf' (js/Buffer. (.-length buf))]
       (. buf (copy buf'))
       (aset buf' (bit-shift-right i 3) new-byte)
-      (Bitfield. buf')))
+      (Bitfield. size buf')))
   ILookup
   (-lookup [this i]
     (-lookup this i nil))
@@ -27,7 +28,7 @@
           buf' (js/Buffer. (.-length buf))]
       (. buf (copy buf'))
       (aset buf' (bit-shift-right i 3) new-byte)
-      (Bitfield. buf')))
+      (Bitfield. size buf')))
   IFn
   (-invoke [this i]
     (-lookup this i))
@@ -36,7 +37,7 @@
 
 (defn of-size [size]
   (let [buf (js/Buffer. (js/Array. (+ 1 (bit-shift-right size 3))))]
-    (Bitfield. buf)))
+    (Bitfield. size buf)))
 
 (let [bf (of-size 1)]
   (assert (not (get bf 0)))
