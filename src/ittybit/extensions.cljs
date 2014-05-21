b2235d708711e1b0143db67d00dc7a59e39ecd5c
Alan O'Donnell <alan.m.odonnell@gmail.com>
2014-05-21 18:53:52 -0400

    Better IEquiv implementation for Buffers

--- src/ittybit/extensions.cljs
+++ src/ittybit/extensions.cljs
@@ -18,9 +18,10 @@
   (-seq [this] (prim-seq this 0))
   IEquiv
   (-equiv [this that]
-    (and (not (nil? that))
-         (= (.-length this) (.-length that))
-         (loop [i 0]
-           (or (= i (.-length this))
-               (and (= (aget this i) (aget that i))
-                    (recur (inc i))))))))
+    (or (identical? this that)
+        (and (not (nil? that))
+             (= (.-length this) (.-length that))
+             (loop [i 0]
+               (or (= i (.-length this))
+                   (and (= (aget this i) (aget that i))
+                        (recur (inc i)))))))))
